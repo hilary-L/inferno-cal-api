36064e047eb69da0de4f559b4f2199bcd15d1790
Hilary Lee <hilary@digintent.com>
2017-01-11 11:50:41 -0600

    Revise server configuration to seed database after 12 hours have passed since last startup. Add gitignore.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from "express";
-import path from "path";
-import bodyParser from "body-parser";
-import mongodb from "mongodb";
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+let mongodb = require('mongodb');
+let faker = require('faker');
 
 let ObjectID = mongodb.ObjectID;
 
@@ -12,9 +13,13 @@ let app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-let db;
+let db, createdDate, timeDifference;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+let today = new Date();
+
+//process.env.MONGODB_URI
+
+mongodb.MongoClient.connect('mongodb://heroku_2s1dc220:peiuo19kkmu4gqt22kaug42egv@ds161048.mlab.com:61048/heroku_2s1dc220', function(err, database) {
 	if(err) {
 		console.log(err);
 		process.exit(1);
@@ -22,7 +27,22 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
 
 	db = database;
 
+
 	console.log("Database connection ready");
+
+	db.collection(TASKS_COLLECTION).aggregate([ { $sample: { size: 1 } } ]).toArray(function(err, docs) {
+		console.log(docs);
+
+		createdDate = docs[0].createdDt;
+		timeDifference = ((today - createdDate) / (1000 * 3600 * 24));
+		console.log(timeDifference);
+		console.log(timeDifference > 0.5);
+
+		if(timeDifference > 0.5) {
+			console.log('been longer than 12 hours');
+			seedDatabase();
+		}
+	});
 
 	let server = app.listen(process.env.PORT || 8080, function() {
 		let port = server.address().port;
@@ -33,6 +53,75 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
 function handleError(res, reason, message, code) {
 	console.log("ERROR: " + reason);
 	res.status(code || 500).json({"error": message});
+}
+
+function seedDatabase() {
+
+	// create some fake data and seed the database on startup
+
+	// drop the database before creating more entries
+
+	db.collection(TASKS_COLLECTION).drop();
+
+	let tasks = [];
+
+	for(i = 0; i < 15; i++) {
+
+		let task = {
+			description: "Play tennis with " + faker.name.firstName(),
+			date: faker.date.between("10/01/2016", "03/30/2017"),
+			address: faker.address.streetAddress(),
+			createdDt: new Date()
+		};
+
+		tasks.push(task);
+
+	}
+
+	for(i = 0; i < 15; i++) {
+
+		let task = {
+			description: "Appointment with " + faker.name.title() + " " + faker.name.lastName(),
+			date: faker.date.between("10/01/2016", "03/30/2017"),
+			address: faker.address.streetAddress(),
+			createdDt: new Date()
+		};
+
+		tasks.push(task);
+
+	}
+
+	for(i = 0; i < 15; i++) {
+
+		let task = {
+			description: "Dinner with " + faker.name.firstName() + " and " + faker.name.firstName(),
+			date: faker.date.between("10/01/2016", "03/30/2017"),
+			address: faker.address.streetAddress(),
+			createdDt: new Date(),
+		};
+
+		tasks.push(task);
+
+	}
+
+	for(i = 0; i < 50; i++) {
+		let task = {
+			description: faker.name.findName() + "'s birthday",
+			date: faker.date.between("01/01/2017", "01/01/2019"),
+			address: faker.address.streetAddress(),
+			createdDt: new Date()
+		}
+
+		tasks.push(task);
+	}
+
+	db.collection(TASKS_COLLECTION).insertMany(tasks, function(err, doc) {
+		if(err) {
+			handleError(res, err.message, "Failed to create new task.");
+		} else {
+			console.log('seeding successful');
+		}
+	});
 }
 
 app.get("/tasks", function(req, res) {
@@ -73,7 +162,7 @@ app.get("/tasks/:id", function(req, res) {
 	});
 });
 
-app.put("/tasks/:id", funciton(req, res) {
+app.put("/tasks/:id", function(req, res) {
 	let updateDoc = req.body;
 	delete updateDoc._id;
 
